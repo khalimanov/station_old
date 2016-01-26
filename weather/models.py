fc02ca28f85810f2c11a87502a5714968e256569
Vadim Iskuchekov <vadim.deus@gmail.com>
2016-01-26 23:58:54 +1000

    Cleanup, add requirements

--- weather/models.py
+++ weather/models.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
+
 class Sensor(models.Model):
-	sensor_name = models.CharField(max_length=100)
+    sensor_name = models.CharField(max_length=100)
+
 
 class Temperature(models.Model):
-	sensor = models.ForeignKey(Sensor)
-	value = models.FloatField()
-	date = models.DateTimeField(auto_now=True)
+    sensor = models.ForeignKey(Sensor)
+    value = models.FloatField()
+    date = models.DateTimeField(auto_now=True)
